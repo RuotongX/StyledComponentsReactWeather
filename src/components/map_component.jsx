bfa9902e559896044d394549382c73f8b2696c88
BakaX <37313808+RuotongX@users.noreply.github.com>
2023-12-09 02:45:47 +1300

    Add Map marker.

--- src/components/map_component.jsx
+++ src/components/map_component.jsx
@@ -1,21 +1,31 @@
 import React, { useContext } from "react";
-import { APIProvider, Map} from "@vis.gl/react-google-maps";
+import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 
-import { LocationContext,LoadingWeatherContext } from "../api/location_weather_get.jsx";
+import {
+  LocationContext,
+  LoadingWeatherContext,
+} from "../api/location_weather_get.jsx";
 
 export default function MapComponent(props) {
   const { location } = useContext(LocationContext);
-  const {isLoading} = useContext(LoadingWeatherContext);
-  
-  
+  const { isLoading } = useContext(LoadingWeatherContext);
+
   return !isLoading ? (
-    <div style={{ height: "50vh", width: "50%" }}>
-        <APIProvider apiKey={process.env.REACT_APP_Map_Key}>
-            <Map
-            zoom={13}
-            center={{ lat: location.latitude, lng: location.longitude }}
-            />
-        </APIProvider>
+    <div style={{ height: "50vh", width: "60%" }}>
+      
+      <APIProvider apiKey={process.env.REACT_APP_Map_Key}>
+        <Map
+          zoom={13}
+          center={{ lat: location.latitude, lng: location.longitude }}
+        >
+          <Marker
+            position={{ lat: location.latitude, lng: location.longitude }}
+            clickable={true}
+            onClick={() => alert("marker was clicked!")}
+            title={"clickable google.maps.Marker"}
+          />
+        </Map>
+      </APIProvider>
     </div>
   ) : (
     <div alt="map_loading"></div>
